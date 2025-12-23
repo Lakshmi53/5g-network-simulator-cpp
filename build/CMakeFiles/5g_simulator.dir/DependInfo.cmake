413890e6fdb960db38052bf363280352954255e8
Lakshmi <lakshmi@Lakshmis-MacBook-Air.local>
2025-12-23 21:32:39 +0530

    Initial 5G network simulator with UE, gNB, and Core

---
+++ build/CMakeFiles/5g_simulator.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/lakshmi/5g-network-simulator/src/core/CoreNetwork.cpp" "CMakeFiles/5g_simulator.dir/src/core/CoreNetwork.cpp.o" "gcc" "CMakeFiles/5g_simulator.dir/src/core/CoreNetwork.cpp.o.d"
+  "/Users/lakshmi/5g-network-simulator/src/gnb/gNB.cpp" "CMakeFiles/5g_simulator.dir/src/gnb/gNB.cpp.o" "gcc" "CMakeFiles/5g_simulator.dir/src/gnb/gNB.cpp.o.d"
+  "/Users/lakshmi/5g-network-simulator/src/main.cpp" "CMakeFiles/5g_simulator.dir/src/main.cpp.o" "gcc" "CMakeFiles/5g_simulator.dir/src/main.cpp.o.d"
+  "/Users/lakshmi/5g-network-simulator/src/ue/UE.cpp" "CMakeFiles/5g_simulator.dir/src/ue/UE.cpp.o" "gcc" "CMakeFiles/5g_simulator.dir/src/ue/UE.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/5g_simulator.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "5g_simulator"
+  "5g_simulator.pdb"
+  "CMakeFiles/5g_simulator.dir/src/core/CoreNetwork.cpp.o"
+  "CMakeFiles/5g_simulator.dir/src/core/CoreNetwork.cpp.o.d"
+  "CMakeFiles/5g_simulator.dir/src/gnb/gNB.cpp.o"
+  "CMakeFiles/5g_simulator.dir/src/gnb/gNB.cpp.o.d"
+  "CMakeFiles/5g_simulator.dir/src/main.cpp.o"
+  "CMakeFiles/5g_simulator.dir/src/main.cpp.o.d"
+  "CMakeFiles/5g_simulator.dir/src/ue/UE.cpp.o"
+  "CMakeFiles/5g_simulator.dir/src/ue/UE.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/5g_simulator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
